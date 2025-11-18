f4c915cd1fc6e4835fb605062bab44970f95f13a
Muaz2004 <muazindris64@gmail.com>
2025-11-18 09:02:39 +0300

    file

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Project, Task
+from .models import CustomUser, Project, Task
 
 admin.site.register(Project)
 admin.site.register(Task)
+admin.site.register(CustomUser)
