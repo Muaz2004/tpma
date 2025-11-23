69bf5ba9115cd3e442f09cb330129bca386409b8
Muaz2004 <muazindris64@gmail.com>
2025-11-23 11:35:29 +0300

    g

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { ApolloProvider } from '@apollo/client';
-import client from './apollo';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
+    <AuthProvider>
       <App />
-    </ApolloProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
