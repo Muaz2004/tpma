267fb26f8c3f882e94fda05cc958d9ce4f3c0d77
Muaz2004 <muazindris64@gmail.com>
2025-11-30 19:01:16 +0300

    temp

---
+++ frontend/src/graphql/loginQuery.js
@@ -0,0 +1,15 @@
+import { gql } from "@apollo/client";
+
+// GraphQL login mutation using username
+export const LOGIN_MUTATION = gql`
+  mutation Login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+      user {
+        id
+        username
+        role
+      }
+    }
+  }
+`;
