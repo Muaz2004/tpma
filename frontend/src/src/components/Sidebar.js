bfb9995f9f6ac65b06246f5424f6023e0267fe1a
Muaz2004 <muazindris64@gmail.com>
2025-12-05 15:20:33 +0300

    side bar made bigger

--- frontend/src/src/components/Sidebar.js
+++ frontend/src/src/components/Sidebar.js
@@ -5,16 +5,14 @@ import { Link } from "react-router-dom";
 const  Sidebar= ()=>{
     return (
 
-        <div  className="w-60 bg-[#3A506B] h-screen flex flex-col items-center py-4 space-y-6 text-white">
+      <div className="w-48 bg-[#3A506B] h-screen flex flex-col items-center py-4 space-y-6 text-white">
+  <Link to="/dashboard" className="text-2xl">🏠</Link>
+  <Link to="/projects" className="text-2xl">📁</Link>
+  <Link to="/tasks" className="text-2xl">📝</Link>
+  <Link to="/profile" className="text-2xl">👤</Link>
+  <Link to="/logout" className="text-2xl">🚪</Link>
+</div>
 
-       <Link to="/dashboard" className="text-2xl">🏠</Link>
-      <Link to="/projects" className="text-2xl">📁</Link>
-      <Link to="/tasks" className="text-2xl">📝</Link>
-      <Link to="/profile" className="text-2xl">👤</Link>
-      <Link to="/logout" className="text-2xl">🚪</Link>
-
-
-        </div>
 
     );
 
