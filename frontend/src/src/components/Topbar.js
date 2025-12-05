c5afcbc7b86e7fd95003d8be41bd5ff094a95369
Muaz2004 <muazindris64@gmail.com>
2025-12-05 15:00:57 +0300

    become larger

--- frontend/src/src/components/Topbar.js
+++ frontend/src/src/components/Topbar.js
@@ -3,11 +3,11 @@ import React from "react";
 
 const Topbar=()=>{
     return(
-        <div className="bg-[#5BC0BE] h-12 flex items-center justify-between px-4">
+        <div className="bg-[#5BC0BE] h-40 flex items-center justify-between px-4">
 
             <div className="text-white font-bold text-lg"> TPMA </div>
 
-            <div className="w-8 h-8 bg-white rounded-full border"></div>
+            <div className="w-20 h-20 bg-white rounded-full border"></div>
 
         </div>
 
