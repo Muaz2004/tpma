274fd73c3a6d358fd6c86febe7724be969e91af6
Muaz2004 <muazindris64@gmail.com>
2025-12-05 15:02:43 +0300

    sidebar become larg

--- frontend/src/src/components/Sidebar.js
+++ frontend/src/src/components/Sidebar.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const  Sidebar= ()=>{
     return (
 
-        <div  className="w-16 bg-[#3A506B] h-screen flex flex-col items-center py-4 space-y-6 text-white">
+        <div  className="w-60 bg-[#3A506B] h-screen flex flex-col items-center py-4 space-y-6 text-white">
 
        <Link to="/dashboard" className="text-2xl">🏠</Link>
       <Link to="/projects" className="text-2xl">📁</Link>
