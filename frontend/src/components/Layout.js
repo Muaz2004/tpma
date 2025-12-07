3c49c6f969ba74a65bdc73ffc15ea195fe785ecc
Muaz2004 <muazindris64@gmail.com>
2025-12-07 11:54:37 +0300

    layout updated

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -2,22 +2,24 @@ import React from "react";
 import Sidebar from "./Sidebar";
 import Topbar from "./Topbar";
 
+const Layout = ({ children }) => {
+  return (
+    <div className="flex min-h-screen bg-[#FCFFE7]">
 
-const Layout=({ children })=>{
-    return(
-        <div  className="flex">
-             <Sidebar /> 
+      <Sidebar />
 
-        <div className="flex-1 flex flex-col">  
+      <div className="flex-1 flex flex-col">
 
-         <Topbar />
-         
-        <main className="flex-1 bg-[#FCFFE7] text-[#1C1C1C] p-4"> {children} </main>
+        <Topbar />
 
-            </div>
-        </div>
+        <main className="flex-1 p-6 text-[#1C1C1C]">
+          {children}
+        </main>
 
-        
-    );
+      </div>
 
-}; export default Layout;
+    </div>
+  );
+};
+
+export default Layout;
