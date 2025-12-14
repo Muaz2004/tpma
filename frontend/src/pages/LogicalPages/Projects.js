a9c826e4b8af432c2d4d0de5b7985ab3b8a403f1
Muaz2004 <muazindris64@gmail.com>
2025-12-14 12:10:31 +0300

    project page started

--- frontend/src/pages/LogicalPages/Projects.js
+++ frontend/src/pages/LogicalPages/Projects.js
@@ -1,15 +1,53 @@
 
 import React, { useContext } from "react";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client";
+
+const GET_PROJECTS = gql`
+  query GetProjects {
+    allProjects {
+      id
+      name
+      description
+      start_date
+      end_date
+      creator
+      tasks{
+        id
+        name
+      }
+    }
+  }
+`;
+
+ 
 
 
 
 const Projects = () => {
+
+
+const { loading, error, data } = useQuery(GET_PROJECTS);
+if (loading) return <p>Loading projects...</p>;
+if (error) return <p>Error: {error.message}</p>;
+if (data.allProjects.length === 0) return <p>No projects found</p>;
+
+
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Hello from Projects page</h1>
-    
-    </div>
-  );
+  <div className="projects-grid">
+    {data.allProjects.map(project => (
+      <div key={project.id} className="project-card">
+        <h2>{project.name}</h2>
+        <p>{project.description}</p>
+        <p>
+          Tasks: {project.tasks.map(task => task.name).join(", ")}
+        </p>
+      </div>
+    ))}
+  </div>
+);
+
+
 };
 
 export default Projects;
