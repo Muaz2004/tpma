74bd4eec3c2534de54ae511233a40e034d0ab51c
Muaz2004 <muazindris64@gmail.com>
2025-12-14 10:14:47 +0300

    app wraped protectedly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,20 +8,71 @@ import Logout from "./pages/Logout";
 import Profile from "./pages/LogicalPages/Profile";
 import Projects from "./pages/LogicalPages/Projects";
 import Tasks from "./pages/LogicalPages/Tasks";
+import ProtectedRoute from "./components/ProtectedRoute";
+
 function App() {
   return (
     <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/manager-dashboard" element={ <Layout> <ManagerDashboard /> </Layout>} />
-          <Route path="/user-dashboard" element={<Layout> <UserDashboard /> </Layout>} />
-          <Route path="/projects" element={ <Layout> <Projects /> </Layout>} />
-          <Route path="/tasks" element={ <Layout><Tasks /> </Layout>} />
-          <Route path="/profile" element={ <Layout> <Profile /> </Layout>} />
-        </Routes>
-    
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/manager-dashboard"
+          element={
+            <ProtectedRoute role="manager">
+              <Layout>
+                <ManagerDashboard />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/user-dashboard"
+          element={
+            <ProtectedRoute role="user">
+              <Layout>
+                <UserDashboard />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/projects"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <Projects />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/tasks"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <Tasks />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Layout>
+                <Profile />
+              </Layout>
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
     </Router>
   );
 }
